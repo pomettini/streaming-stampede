5bc05e60a0b48fde11db6b71d4c9765753256e53
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2017-11-09 22:25:08 +0100

    Added some UI and state machine logic

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ use ggez::event;
 use ggez::event::*;
 use ggez::{GameResult, Context};
 use ggez::graphics;
-use ggez::graphics::{Point};
-use ggez::graphics::{Rect};
+use ggez::graphics::{Point, Rect, DrawMode, Color};
 use ggez::timer;
 use std::time::Duration;
 use ggez::audio;
@@ -19,7 +18,8 @@ use std::collections::HashMap;
 const WINDOW_W: u32 = 1024;
 const WINDOW_H: u32 = 768;
 
-enum GameStates
+#[derive(PartialEq)]
+enum GameState
 {
     Start,
     Tutorial,
@@ -83,7 +83,7 @@ impl Pokemon
         self.spr_frame += 0.25;
 
         // Move to the next sprite
-        if(self.spr_frame as u8 >= self.spr_num_frames)
+        if self.spr_frame as u8 >= self.spr_num_frames
         {
             self.spr_frame = 0.;
         }
@@ -209,21 +209,39 @@ fn spawn_pokemon(ptype: PokemonType, pok_list: &mut HashMap<PokemonType, u8>) ->
     }
 }
 
+struct InputState 
+{
+    space: bool,
+}
+
+impl InputState
+{
+    fn default() -> Self 
+    {
+        InputState 
+        {
+            space: false,
+        }
+    }
+}
+
 struct MainState 
 {
     assets: Assets,
     pokemon: Vec<Pokemon>,
     pok_list: HashMap<PokemonType, u8>,
     font: graphics::Font,
-    title: graphics::Text,
-    state: u32,
+    state: GameState,
+    input: InputState,
+    countdown: f32,
+    is_pressing: bool,
 }
 
 impl MainState 
 {
     fn new(ctx: &mut Context) -> GameResult<MainState> 
     {
-        ctx.print_resource_stats();
+        // ctx.print_resource_stats();
         
         graphics::set_background_color(ctx, (0, 0, 0, 255).into());
 
@@ -231,7 +249,7 @@ impl MainState
         let mut pokemon = vec![];
         let mut pok_list = HashMap::new();
         let font = graphics::Font::new(ctx, "/pacifico.ttf", 80)?;
-        let title = graphics::Text::new(ctx, "Streaming Stampede", &font)?;
+        let mut countdown = 3.49;
 
         // for _ in 0..30 
         // {
@@ -241,13 +259,24 @@ impl MainState
         for _ in 0..5 
         {
             pokemon.push(spawn_pokemon(PokemonType::Slugma, &mut pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Magcargo, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Voltorb, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Electrode, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Pichu, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Pikachu, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Togepi, pok_list));
-            // pokemon.push(spawn_pokemon(PokemonType::Doduo, pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Magcargo, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Voltorb, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Electrode, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Pichu, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Pikachu, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Togepi, &mut pok_list));
+
+            // Lol
+            pokemon.push(spawn_pokemon(PokemonType::Doduo, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Doduo, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Doduo, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Doduo, &mut pok_list));
+            pokemon.push(spawn_pokemon(PokemonType::Doduo, &mut pok_list));
+        }
+
+        for (pokemon, count) in &mut pok_list 
+        {
+            println!("{:?} counted: {}", pokemon, count);
         }
 
         // pokemon.push(spawn_pokemon(PokemonType::Diglett));
@@ -259,11 +288,24 @@ impl MainState
             pokemon: pokemon,
             pok_list: pok_list,
             font: font,
-            title: title,
-            state: 0,
+            state: GameState::Start,
+            input: InputState::default(),
+            countdown: countdown,
+            is_pressing: false,
         };
 
         Ok(s)
+    }
+
+    fn change_state(&mut self, state: GameState)
+    {
+        self.reset_countdown();
+        self.state = state;
+    }
+
+    fn reset_countdown(&mut self)
+    {
+        self.countdown = 3.49;
     }
 }
 
@@ -272,10 +314,61 @@ impl event::EventHandler for MainState
     // Move stuff
     fn update(&mut self, ctx: &mut Context, dt: Duration) -> GameResult<()> 
     {
-        // Moving every Pokemon
-        for p in &mut self.pokemon 
+        if self.state == GameState::Start
         {
-            p.update();
+            if self.input.space
+            {
+               //self.state = GameState::Tutorial;
+               self.change_state(GameState::Tutorial);
+            }
+        }
+
+        if self.state == GameState::Tutorial
+        {
+            if self.input.space
+            {
+               self.change_state(GameState::Countdown);
+            }
+        }
+
+        if self.state == GameState::Countdown
+        {
+            let duration = timer::get_delta(ctx);
+            let delta_time = timer::duration_to_f64(duration);
+            self.countdown -= delta_time as f32;
+
+            println!("{}", delta_time);
+
+            if self.countdown <= 0.5
+            {
+                self.change_state(GameState::Question);
+            }
+        }
+
+        if self.state == GameState::Question
+        {
+            let duration = timer::get_delta(ctx);
+            let delta_time = timer::duration_to_f64(duration);
+            self.countdown -= delta_time as f32;
+
+            if self.countdown <= 0.5
+            {
+                self.change_state(GameState::Race);
+            }
+        }
+
+        if self.state == GameState::Race
+        {
+            // Moving every Pokemon
+            for p in &mut self.pokemon 
+            {
+                p.update();
+            }
+        }
+
+        if self.state == GameState::Answer
+        {
+
         }
 
         Ok(())
@@ -285,58 +378,112 @@ impl event::EventHandler for MainState
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> 
     {
         let assets = &mut self.assets;
+        let font = &mut self.font;
 
         // Clearing the screen
         graphics::clear(ctx);
 
-        let bg_width = assets.field_bg.width();
-        let bg_height = assets.field_bg.height();
-        
-        // I draw the background
-        let bg_draw_options = graphics::DrawParam
-        { 
-            src: Rect { x: -0.0, y: 0.0, w: 1.0, h: 1.0 },
-            dest: Point 
-            { 
-                x: WINDOW_W as f32 / 2.0, 
-                y: WINDOW_H as f32 / 2.0 
-            },
-            rotation: 0.0,
-            scale: Point 
-            { 
-                x: WINDOW_W as f32 / bg_width as f32, 
-                y: WINDOW_H as f32 / bg_height as f32, 
-            },
-            offset: Point { x: 0.0, y: 0.0 },
-            shear: Point { x: 0.0, y: 0.0 },
-        };
-        graphics::draw_ex(ctx, &assets.field_bg, bg_draw_options);
-        
-        // if self.state == 0
-        // {
-        //     let text_pos_x: f32 = WINDOW_W as f32 / 2.0;
-        //     let text_pos_y: f32 = WINDOW_H as f32 / 4.0;
-
-        //     graphics::draw(ctx, &self.title, Point { x: text_pos_x, y: text_pos_y }, 0.0)?;
-        // }
-
-        // if self.state < GAME_STATE_PLAY 
-        // {
-        //     for id in 0..30 
-        //     {
-        //         self.pokemon[id].draw(ctx);
-        //     }
-        // }
-
-        for p in &self.pokemon 
+        if self.state == GameState::Start
         {
-            draw_pokemon(assets, ctx, p)?;
+            draw_game_background(assets, ctx);
+            // draw_game_window(ctx);
+
+            let title = graphics::Text::new(ctx, "Streaming Stampede", &font)?;
+
+            let text_pos_x: f32 = WINDOW_W as f32 / 2.0;
+            let text_pos_y: f32 = WINDOW_H as f32 / 2.0;
+
+            graphics::draw(ctx, &title, Point { x: text_pos_x, y: text_pos_y }, 0.0)?;
+        }
+
+        if self.state == GameState::Tutorial
+        {
+            draw_game_background(assets, ctx);
+
+            let title = graphics::Text::new(ctx, "Tutorial", &font)?;
+            
+            let text_pos_x: f32 = WINDOW_W as f32 / 2.0;
+            let text_pos_y: f32 = WINDOW_H as f32 / 2.0;
+
+            graphics::draw(ctx, &title, Point { x: text_pos_x, y: text_pos_y }, 0.0)?;
+        }
+
+        if self.state == GameState::Countdown
+        {
+            draw_game_background(assets, ctx);
+
+            let round = self.countdown.round();
+            let timer = round.to_string();
+            let title = graphics::Text::new(ctx, &timer, &font)?;
+            
+            let text_pos_x: f32 = WINDOW_W as f32 / 2.0;
+            let text_pos_y: f32 = WINDOW_H as f32 / 2.0;
+
+            graphics::draw(ctx, &title, Point { x: text_pos_x, y: text_pos_y }, 0.0)?;
+        }
+
+        if self.state == GameState::Question
+        {
+            draw_game_background(assets, ctx);
+
+            let title = graphics::Text::new(ctx, "Count that Pokemon", &font)?;
+
+            let text_pos_x: f32 = WINDOW_W as f32 / 2.0;
+            let text_pos_y: f32 = WINDOW_H as f32 / 4.0;
+
+            graphics::draw(ctx, &title, Point { x: text_pos_x, y: text_pos_y }, 0.0)?;
+
+            draw_pokemons_on_answer_screen(assets, ctx, PokemonType::Voltorb)?;
+        }
+
+        if self.state == GameState::Race
+        {
+            draw_game_background(assets, ctx);
+
+            for p in &self.pokemon 
+            {
+                draw_pokemon(assets, ctx, p)?;
+            }
+        }
+
+        if self.state == GameState::Answer
+        {
+            
         }
 
         // Blitting! \m/
         graphics::present(ctx);
 
         Ok(())
+    }
+
+    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, _repeat: bool) 
+    {
+        match keycode 
+        {
+            Keycode::Space => 
+            {
+                self.input.space = true;
+            }
+
+            // Keycode::Escape => ctx.quit().unwrap(),
+
+            _ => (),
+        }
+    }
+
+
+    fn key_up_event(&mut self, keycode: Keycode, _keymod: Mod, _repeat: bool) 
+    {
+        match keycode 
+        {
+            Keycode::Space => 
+            {
+                self.input.space = false;
+            }
+
+            _ => (),
+        }
     }
 }
 
@@ -391,9 +538,9 @@ impl Assets
         })
     }
 
-    fn pokemon_sprite(&mut self, pokemon: &Pokemon) -> &mut Vec<graphics::Image> 
+    fn pokemon_sprite(&mut self, ptype: PokemonType) -> &mut Vec<graphics::Image> 
     {
-        match pokemon.ptype 
+        match ptype 
         {
             PokemonType::Slugma => &mut self.slugma_spr,
             PokemonType::Magcargo => &mut self.magcargo_spr,
@@ -411,11 +558,11 @@ impl Assets
 
 fn draw_pokemon(assets: &mut Assets, ctx: &mut Context, pokemon: &Pokemon) -> GameResult<()> 
 {
-    let image = assets.pokemon_sprite(pokemon);
+    let image = assets.pokemon_sprite(pokemon.ptype.clone());
     let pos = pokemon.pos;
     let frame = pokemon.spr_frame;
 
-    // I draw the background
+    // I do that to scale the Pokemon to 2x
     let sprite_options = graphics::DrawParam
     { 
         src: Rect { x: -0.0, y: 0.0, w: 1.0, h: 1.0 },
@@ -431,6 +578,64 @@ fn draw_pokemon(assets: &mut Assets, ctx: &mut Context, pokemon: &Pokemon) -> Ga
     };
 
     graphics::draw_ex(ctx, &image[frame as usize], sprite_options)
+}
+
+fn draw_pokemons_on_answer_screen(assets: &mut Assets, ctx: &mut Context, ptype: PokemonType) -> GameResult<()>
+{
+    let image = assets.pokemon_sprite(ptype);
+    let pos = Point { x: WINDOW_W as f32 / 2.0, y: WINDOW_H as f32 / 2.0 };
+
+    const scale_factor: f32 = 4.0;
+
+    // I scale the Pokemon to [scale_factor]
+    let sprite_options = graphics::DrawParam
+    { 
+        src: Rect { x: -0.0, y: 0.0, w: 1.0, h: 1.0 },
+        dest: pos,
+        rotation: 0.0,
+        scale: Point 
+        { 
+            x: scale_factor, 
+            y: scale_factor, 
+        },
+        offset: Point { x: 0.0, y: 0.0 },
+        shear: Point { x: 0.0, y: 0.0 },
+    };
+
+    graphics::draw_ex(ctx, &image[2], sprite_options)
+}
+
+fn draw_game_background(assets: &mut Assets, ctx: &mut Context) -> GameResult<()>
+{
+    let bg_width = assets.field_bg.width();
+    let bg_height = assets.field_bg.height();
+            
+    let bg_draw_options = graphics::DrawParam
+    { 
+        src: Rect { x: -0.0, y: 0.0, w: 1.0, h: 1.0 },
+        dest: Point 
+        { 
+            x: WINDOW_W as f32 / 2.0, 
+            y: WINDOW_H as f32 / 2.0 
+        },
+        rotation: 0.0,
+        scale: Point 
+        { 
+            x: WINDOW_W as f32 / bg_width as f32, 
+            y: WINDOW_H as f32 / bg_height as f32, 
+        },
+        offset: Point { x: 0.0, y: 0.0 },
+        shear: Point { x: 0.0, y: 0.0 },
+    };
+
+    graphics::draw_ex(ctx, &assets.field_bg, bg_draw_options)
+}
+
+fn draw_game_window(ctx: &mut Context) -> GameResult<()>
+{
+    graphics::set_background_color(ctx, Color { r: 255., g: 255., b: 0., a: 127. });
+    let rect = Rect { x: 0., y: 0., w: WINDOW_W as f32 * 2.0, h: WINDOW_H as f32 * 2.0 };
+    graphics::rectangle(ctx, DrawMode::Fill, rect)
 }
 
 fn main() 
